7854119726e348f315ffd7c2ced866174b2c2315
Ravikumar Gudipati <ravi@itsacheckmate.com>
2019-12-21 18:36:14 -0800

    Specs added

--- spec/lib/google_maps_api_spec.rb
+++ spec/lib/google_maps_api_spec.rb
@@ -1,39 +1,30 @@
-describe GoogleMapsApi do
-  let!(:config) { {
-    api_key: "AIzaSyCI33WirMl2FJlzwi5vey0C8T63qnFh4Jg"
-    type: 'atm'
-    language: 'english'
-    output: 'json'
-    num_locations: 20
-  } }
- 
-  describe '#base_url' do
-    gma = GoogleMapsApi.new(config)
+require 'google_maps_api'
+require 'rails_helper'
 
+describe GoogleMapsApi::Search do
+  let(:config) { YAML.load(File.read("#{Rails.root}/config/maps_attributes.yml")) }
+
+  describe '#base_url' do
+    gma = GoogleMapsApi::Search.new(config)
     url = "https://maps.googleapis.com/"
 
     expect(@gma.base_url).to eq(url)
   end
 
   describe '#get_locations' do
-
     it 'returns xml output format' do
-       gma =  GoogleMapsApi.new(config.merge({ out: 'xml'}))
-
+       gma =  GoogleMapsApi::Search.new(config['customerA'])
        res = "<xml>something</xml>"
 
        expect(gma). to receive(:get_locations).and_return(res)
     end
 
     it 'returns json output format' do
-       gma =  GoogleMapsApi.new(config.merge({ out: 'json'}))
-
+       gma =  GoogleMapsApi::Search.new(config['customerB'])
        res = "[ {locations: 'xyz'} ]"
 
        expect(gma). to receive(:get_locations).and_return(res)
     end
-
   end
 end
-
 
